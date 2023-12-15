e9a0ed6087d49f285a3dcdf791ef91588d91f5a3
Mohamed Cheikh Sidiya <mohamedcheikhsidiya77@gmail.com>
2023-12-15 15:52:43 +0100

    small refactor

--- firebase_certif.py
+++ firebase_certif.py
@@ -7,10 +7,10 @@ if not downloads_path.exists():
     downloads_path = Path.home() / "Téléchargements"
 
 
-firebase_key = downloads_path / "antitriche-firebase-adminsdk.json"
+firestore_certif = downloads_path / "antitriche-firebase-adminsdk.json"
 
 # déplace le fichier vers le répertoire courant (du projet) si le fichier existe
-if firebase_key.exists():
-    shutil.move(firebase_key, Path.cwd())
+if firestore_certif.exists():
+    shutil.move(firestore_certif, Path.cwd())
 else:
     print(f"Le fichier n'a pas été trouvé dans {downloads_path}")
