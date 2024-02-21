f2a4226271efe057abc2e9a37627f4f963a23e0e
Sadegh Abderrahim <sadeghabderahim@gmail.com>
2024-02-21 11:11:43 +0100

    .

--- src/firestore_db.py
+++ src/firestore_db.py
@@ -87,7 +87,7 @@ class FirestoreDB:
             return doc_ref.to_dict()
         return None
 
-    def get_random_question(self):
+    def get_random_question(self, langage):
         """Récupère une question aléatoire pour un langage donné"""
         questions = self.get_doc_list("Questions")
         return random.choice(questions)
