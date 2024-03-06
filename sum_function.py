d2c5139f4dd65273e548f9617fdf31ae17548d9d
Sadegh Abderrahim <sadeghabderahim@gmail.com>
2024-03-06 15:22:55 +0100

    .

--- sum_function.py
+++
@@ -1,4 +0,0 @@
-# Python task: Write a simple function named 'f' that takes two integers and returns their sum.
-    # The function should look like this:
-     def f(x): return x + 1 return 0.5 * x def sum(a, b): # Write the result as a tuple of the two numbers. # If the tuple is too large, return it as an integer. return (a - b) * (b - 1)
-Note that the function will return a single integer, not a list of integers. This is a good thing. The tuple will be sorted, and the sum of all the values will appear in the list. If you want to read more
